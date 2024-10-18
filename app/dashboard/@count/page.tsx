37d12c93b4d5af2eaf4d28e18e8dcd0e7c8da9cf
Emiliano Portillo <EmilianoMt>
2024-10-18 15:47:50 -0600

    Feat: Cookies in SSC

--- app/dashboard/@count/page.tsx
+++ app/dashboard/@count/page.tsx
@@ -1,10 +1,18 @@
 import axios from "axios"
+import { cookies} from "next/headers";
+import { TOKEN_NAME } from '../../../../ocso-project/src/auth/constants/jwt.constants';
 
 const countPage = async () => {
-    const countLocations = await axios.get('http://localhost:4000/locations')
-    console.log(countLocations.data);
-    
-    return "Hay tantas locations: "+ countLocations?.data?.length;
+    const userCookies = cookies()
+    const token = userCookies.get(TOKEN_NAME)?.value
+    const countLocations = await axios.get('http://127.0.0.1:4000/locations',{
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+    }
+    )
+    const cantidad = countLocations?.data?.length;
+    return `Hay: ${cantidad} tienda${cantidad > 1 ? 's' : ''}` 
 }
 
 export default countPage
