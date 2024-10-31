c02d9d6090da1db69240dd8d488a333286dbe2b1
Emiliano Portillo <EmilianoMt>
2024-10-30 20:30:58 -0600

    Feat: Managers stats

---
+++ app/dashboard/managers/[id]/_components/ManagerCard.tsx
@@ -0,0 +1,45 @@
+import { Manager } from "@/entities";
+import { Card, CardHeader, Divider, CardBody } from "@nextui-org/react";
+import Link from "next/link";
+
+export default function ManagerCard({ manager }: { manager: Manager }) {
+  return (
+    <Card className="mx-20 py-2 text-center">
+      <CardHeader>
+        <p className="w-full">
+          <b className="text-4xl">{manager.managerFullName}</b>{" "}
+        </p>
+      </CardHeader>
+      <Divider />
+      <CardBody className=" items-center">
+        <div>
+          <p className="w-full">
+            Email: <b>{manager.managerEmail}</b>{" "}
+          </p>
+          <p className="w-full">
+            Telefono: <b>{manager.managerPhoneNumber}</b>
+          </p>
+          {manager.location ? (
+            <>
+              <p className="w-full">
+                Tienda:{" "}
+                <Link
+                  href={{
+                    pathname: `/dashboard`,
+                    query:{
+                        store: manager?.location?.locationId
+                    }
+                  }}
+                >
+                  <b className="underline">{manager.location?.locationName}</b>
+                </Link>
+              </p>
+            </>
+          ) : (
+            <p>No tiene tienda</p>
+          )}
+        </div>
+      </CardBody>
+    </Card>
+  );
+}
--- app/dashboard/managers/layout.tsx
+++ app/dashboard/managers/layout.tsx
@@ -1,17 +1,23 @@
-import ManagerCards from "./[id]/_components/ManagerCards";
+import ManagerCards from "./_components/ManagerCards";
 
 export default function LayoutManagers({
-  children,
+  children, count
 }: {
   children: React.ReactNode;
+  count: React.ReactNode;
 }) {
   return (
     <>
       <div className="w-4/12 max-h-[90vh] h-[90vh] overflow-hidden overflow-y-auto">
-        <ManagerCards></ManagerCards>
+        <ManagerCards/>
       </div>
-      <div>
-        {children}
+      <div className="w-7/12 flex flex-col justify-center items-center gap-10">
+        <div>
+          {children}
+        </div>
+        <div>
+          {count}
+        </div>
       </div>
     </>
   );
