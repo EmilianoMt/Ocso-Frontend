abf9576b6af9d0fc76196c9acd58a7c1c276ba47
Emiliano Portillo <EmilianoMt>
2024-10-31 11:00:10 -0600

    Styles changed

--- app/dashboard/managers/[id]/_components/ManagerCard.tsx
+++ app/dashboard/managers/[id]/_components/ManagerCard.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function ManagerCard({ manager }: { manager: Manager }) {
   return (
-    <Card className="mx-20 py-2 text-center">
+    <Card className="mx-20 py-2 text-center w-full">
       <CardHeader>
         <p className="w-full">
           <b className="text-4xl">{manager.managerFullName}</b>{" "}
@@ -18,6 +18,9 @@ export default function ManagerCard({ manager }: { manager: Manager }) {
           </p>
           <p className="w-full">
             Telefono: <b>{manager.managerPhoneNumber}</b>
+          </p>
+          <p className="w-full">
+            Salario: <b>{manager.managerSalary}</b>
           </p>
           {manager.location ? (
             <>
