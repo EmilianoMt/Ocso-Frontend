fe59a67bca9be420983ccef0b7c3cee801fe6677
Emiliano Portillo <emiliano.portillo1331@gmail.com>
2024-11-30 16:21:31 -0600

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
 import { TOKEN_NAME } from "../ocso-project/src/auth/constants/jwt.constants";
+import * as jose from 'jose'
 
-export default function Middleware(req: NextRequest){
+
+export default async function Middleware(req: NextRequest){
     const token = req.cookies.get(TOKEN_NAME)?.value;
     if(req.nextUrl.pathname.startsWith('/dashboard')){
         if (!token) {
             return NextResponse.redirect(new URL('/login', req.url))
+        }
+    }
+    if (req.nextUrl.pathname.startsWith('/managers')) {
+        if(!token){
+            return NextResponse.redirect(new URL('/login', req.url))
+        }
+        try {
+            const {payload} = await jose.jwtVerify(token, new TextEncoder().encode(TOKEN_NAME));            
+            if (!Array.isArray(payload.userRoles) || !payload.userRoles.includes('Manager') || !payload.userRoles.includes('Admin')) {
+                return NextResponse.redirect(new URL('/Home', req.url));
+            }
+        } catch (error) {
+            return NextResponse.redirect(new URL('/Login', req.url));
+        }
+    }
+    if (req.nextUrl.pathname.startsWith('/providers')) {
+        if(!token){
+            return NextResponse.redirect(new URL('/login', req.url))
+        }
+        try {
+            const {payload} = await jose.jwtVerify(token, new TextEncoder().encode(TOKEN_NAME));            
+            if (!Array.isArray(payload.userRoles) || !payload.userRoles.includes('Manager') || !payload.userRoles.includes('Admin')) {
+                return NextResponse.redirect(new URL('/Home', req.url));
+            }
+        } catch (error) {
+            return NextResponse.redirect(new URL('/Login', req.url));
         }
     }
     if (req.nextUrl.pathname === '/') {
